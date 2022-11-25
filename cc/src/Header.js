48487f422e2ab435a73034d64439e96cfbaf4d69
Павло Квашук <33426981+pavlikkvashuk@users.noreply.github.com>
2022-11-25 21:39:47 +0200

    new project

---
+++ cc/src/Header.js
@@ -0,0 +1,13 @@
+import s from './Header.css'
+const Header = () => {
+    return (
+
+        <header className={s.header}>
+            
+            <span>Actual Price</span>
+
+        </header>
+    )
+
+}
+export default Header;
